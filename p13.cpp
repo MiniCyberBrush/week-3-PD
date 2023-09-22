f7d32e3b1b02d3a011ad83015d79858c347a17ea
Minahil Imran <144251674+MiniCyberBrush@users.noreply.github.com>
2023-09-22 20:54:53 +0500

    Add files via upload

---
+++ p13.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+main() {
+    int num, sum;
+    sum = 0;
+    cout << "Enter the first integer: ";
+    cin >> num;
+    sum += num;
+
+    cout << "Enter the second integer: ";
+    cin >> num;
+    sum += num;
+
+    cout << "Enter the third integer: ";
+    cin >> num;
+    sum += num;
+
+    cout << "Enter the fourth integer: ";
+    cin >> num;
+    sum += num;
+
+    cout << "Enter the fifth integer: ";
+    cin >> num;
+    sum += num;
+
+    cout << "The sum of the entered integers is: " << sum ;
+
+}
+
+
+
+
